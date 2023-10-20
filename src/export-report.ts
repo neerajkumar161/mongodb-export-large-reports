b8bd0e14fa0750cdba6b3725ace44421783f42d4
neerajkumar161 <ennkay161@gmail.com>
2023-10-20 21:45:31 +0530

    chore: export-report for generating csv

--- src/export-report.ts
+++ src/export-report.ts
@@ -1,15 +1,9 @@
 import 'dotenv/config'
-import { Document, MongoClient, ServerApiVersion } from 'mongodb'
-import { Transform, Writable } from 'node:stream'
+import { AggregateOptions, Document, MongoClient, ServerApiVersion } from 'mongodb'
+import { createWriteStream } from 'node:fs'
+import { Transform } from 'node:stream'
 import { pipeline } from 'node:stream/promises'
-
-const client = new MongoClient(process.env.DB_URI!, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true
-  }
-})
+import { TransformCallback } from 'stream'
 
 const pipeLine: Document[] = [
   {
@@ -21,65 +15,66 @@ const pipeLine: Document[] = [
 ]
 
 async function* getDataFromDB() {
+  const client = new MongoClient(process.env.DB_URI!, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true
+    }
+  })
+
   const collection = client.db('mydb1').collection('users')
 
   const usersCursor = collection.aggregate(pipeLine)
 
   for await (const user of usersCursor) {
-    // yield Buffer.from(`${user}`)
     yield JSON.stringify(user)
   }
 }
 
-function tranformData() {}
-let counter = 0;
-await pipeline(
-  getDataFromDB(),
-  new Transform({
-    transform(chunk, encoding, callback) {
-      // format the data according to the needs
-      console.log(JSON.parse(chunk.toString()), ++counter)
-      callback(null, chunk)
-    }
-  }),
-  new Writable({
-    write(chunk, encoding, callback) {
-      // console.log(chunk)
-      callback()
-    }
-  })
-)
-
-type ReportOptions = {db_uri: string, database: string, collection: string, mongodbPipeline: Document[]}
-
-
-class ExportReport extends Transform {
-  private options: ReportOptions
-  private tranformFn: () => void
- 
-  constructor(options: ReportOptions) {
-    super()
-    this.options = options
-  }
-
-  private async *connectDB() {
-    const client = new MongoClient(this.options.db_uri, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true
-      }
-    })
-
-    const collection = client.db(this.options.database).collection(this.options.collection)
-
-    const usersCursor = collection.aggregate(this.options.mongodbPipeline)
-
-    for await (const user of usersCursor) {
-      // yield Buffer.from(`${user}`)
-      yield JSON.stringify(user)
-    }
-  }
-
-  
+type ReportOptions = {
+  db_uri: string
+  database: string
+  collection: string
+  mongodbPipeline: Document[]
+  aggregateOptions?: AggregateOptions
 }
+
+class TranformData extends Transform {
+  private tranformFn: (data: any) => any
+
+  constructor(tranformFn: (data: any) => any) {
+    super()
+    this.tranformFn = tranformFn
+  }
+
+  private convertTranformedData(item: any) {
+    // if file type is csv, then we will add break line, and send next pipe
+    
+    const object = this.tranformFn(item)
+    const finalValue = Object.values(object).reduce((prev, curr) => `${prev},${curr}`)
+    return `${finalValue}\n`
+  }
+
+  _transform(chunk: any, encoding: BufferEncoding, callback: TransformCallback): void {
+    this.push(this.convertTranformedData(chunk))
+
+    callback()
+  }
+}
+
+const tranformStream = new TranformData((data) => {
+  console.log('Data', JSON.parse(data.toString()))
+  const jsonData = JSON.parse(data.toString())
+  return {
+    ...jsonData,
+    age: jsonData.age.toString()
+  }
+})
+
+export async function exportReport() {
+  await pipeline(getDataFromDB(), tranformStream, createWriteStream('test.csv'))
+}
+
+
+await exportReport()
